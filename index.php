f0eec208a714d75248eec1ffa1bdb26563cb75e7
Obinna Johnphill <obinna.johnphill@enta.net>
2017-07-28 15:01:04 +0100

    a new index page

--- index.php
+++ index.php
@@ -5,3 +5,15 @@
  * Date: 28/07/17
  * Time: 14:59
  */
+
+
+include_once 'vendor/obinna/hello/src/HelloWorld2.php';
+
+
+    $test = new HelloWorld();
+    $hello = $test->world();
+    echo  $hello;
+
+
+
+?>
