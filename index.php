f3901a16a1aa0ef16201ef39e23fe68223e86292
Obinna Johnphill <obinna.johnphill@enta.net>
2017-07-28 15:00:28 +0100

    a new class

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: obinnajohnphill
+ * Date: 28/07/17
+ * Time: 14:59
+ */
---
+++ src/HelloWorld2.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: obinnajohnphill
+ * Date: 28/07/17
+ * Time: 11:17
+ */
+
+
+class HelloWorld
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+
+}
