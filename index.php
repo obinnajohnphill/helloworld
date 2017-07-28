c86bc3d14e54969ece2248162b13baeefe09bd3a
Obinna Johnphill <obinna.johnphill@enta.net>
2017-07-28 14:21:02 +0100

    added index page

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: obinnajohnphill
+ * Date: 28/07/17
+ * Time: 14:17
+ */
+
+include_once 'src/HelloWorld.php';
+
+
+    $test = new HelloWorld();
+    $hello = $test->world();
+    echo  $hello;
+
+
+
+?>
