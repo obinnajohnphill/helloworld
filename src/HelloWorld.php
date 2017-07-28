0d31db49c5ad3a604682511dd8425fc7b8f6c777
Obinna Johnphill <obinna.johnphill@enta.net>
2017-07-28 11:32:59 +0100

    First commit

---
+++ src/HelloWorld.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: obinnajohnphill
+ * Date: 28/07/17
+ * Time: 11:17
+ */
+
+namespace App;
+
+
+class HelloWorld
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+
+}
